856fab5c3fe3cd3375af96fc1c15f25f28ebccd0
Yash Bhavsar <yashbhavsar02@gmail.com>
2024-07-08 12:00:11 +0530

    Implement verify code route

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,67 @@
+import dbConnection from "@/lib/dbConnection";
+import UserModel from "@/model/user.model";
+// import { z } from "zod";
+// import { userNameValidation } from "@/schemas/signupSchema";
+
+export async function POST(request: Request) {
+	try {
+		await dbConnection();
+		const { username, code } = await request.json();
+
+		const decodedUsername = decodeURIComponent(username);
+
+		const user = await UserModel.findOne({ username: decodedUsername });
+
+		if (!user) {
+			return Response.json(
+				{
+					success: false,
+					message: "User not found",
+				},
+				{ status: 500 }
+			);
+		}
+
+		const isCodeValid = user.verifyCode === code;
+
+		const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+		if (isCodeValid && isCodeNotExpired) {
+			user.isVerified = true;
+			await user.save();
+
+			return Response.json(
+				{
+					success: true,
+					message: "Account verified",
+				},
+				{ status: 200 }
+			);
+		} else if (!isCodeNotExpired) {
+			return Response.json(
+				{
+					success: false,
+					message: "Verification Code has expired. Please sign up again",
+				},
+				{ status: 400 }
+			);
+		} else {
+			return Response.json(
+				{
+					success: false,
+					message: "Incorrect verification code",
+				},
+				{ status: 400 }
+			);
+		}
+	} catch (error) {
+		console.error("Error verifying User", error);
+		return Response.json(
+			{
+				success: false,
+				message: "Error verifying User",
+			},
+			{ status: 500 }
+		);
+	}
+}
