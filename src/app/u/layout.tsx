445267e7d357afbfbdbb3bf7d88241d5c0ff6c18
Yash Bhavsar <yashbhavsar02@gmail.com>
2024-10-15 11:55:42 +0530

    Initial commit

---
+++ src/app/u/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Anonymous feedback',
+  description: 'Send the anonymous feedback',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
